1f3b02e5bab6fc2908366382a8a9bf1c1b642fbe
AngelJostar <angelrojas@ciencias.unam.mx>
2021-05-22 20:01:42 -0500

    commit all 3

---
+++ propositos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once 'includes/tags/google_tag_manager_head.php';?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" rel="preload" href="/css/style.css" media="all">
+    <link rel="stylesheet" rel="preload" href="/css/normalize.css" media="all">
+</head>
+<?php include_once 'includes/tags/google_tag_manager_body.php';?>
+    
+    <?php include_once 'includes/templates/header.php';?>
+    <div class="contenedor-banner">
+        <div class="banner-propositos">
+            <img src="/img/farmasoma-propositos.jpg" alt="Banner farmasoma">
+        </div>
+        <div class="decoracion-larga">
+            <img src="/img/DECORACION-LARGA.png" alt="Decoración Larga">
+        </div>
+        <div class="boton-contactanos-header-propositos">
+            <button class="custom-btn btn-14">Contáctanos</button>
+        </div>
+    </div>
+    <div class="box-contenedor-propositos">    
+        <div class="pad-top-2rem">
+            <h2 class="font-upper">Misión</h2>
+            <p>Ofrecer una alternativa de distribución de medicamentos de todo tipo, así como la distribución de mezclas estériles de manera profesional, especializada y vanguardia para dar el mejor servicio a los pasientes con todo tipo de padecimientos en México. Sirviendo a la sociedad mexicana de manera responsable, profesional y ética; pues los pacientes nos confían lo más preciado que pueden tener: su salud. </p>
+        </div>
+        <div class="pad-top-2rem">
+            <h2 class="font-upper">Visión</h2>
+            <p>Ser la alternativa de distribución de meticamentos de todo tipo, así como la distribución de mezclas estériles mejor integrada y con cobertura nacional en todo México, reconocida por la alta preparación y capacidad profesional de su equipo y el mejor servicio y atención de la más alta calidad para tratar todo tipo de padecimientos.</p>
+        </div>
+        <div class="pad-top-2rem">
+            <h2 class="font-upper">Valores</h2>
+            <p>FARMASOMA trabaja fundamentalmente con la más alta Ética profesional, Compromiso, Seguridad, Calidad, Honestidad y Respeto por la gente y nuestros colaboradores.</p>
+        </div>
+    </div>
+    
+
+
+    <script src="https://kit.fontawesome.com/3919625d45.js" crossorigin="anonymous"></script><script src="https://kit.fontawesome.com/3919625d45.js" crossorigin="anonymous"></script>
+</body>
+</html>
